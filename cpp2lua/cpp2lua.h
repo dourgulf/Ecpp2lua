6813138cb650b3d7614c9037c3e0c83f38077d60
dawenhing <dawenhing@gmail.com>
2017-12-27 19:08:28 +0800

    add

---
+++ cpp2lua/cpp2lua.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef CPP2LUA_H__
+#define CPP2LUA_H__
+
+#include "lua.hpp"
+
+namespace cpp2lua
+{
+
+};
+
+#endif
---
+++ vcbuild/main.cpp
@@ -0,0 +1,8 @@
+#include "cpp2lua.h"
+#include <stdlib.h>
+
+int main(void)
+{
+	system("pause");
+	return 0;
+}
